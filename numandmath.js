9ea9c9b4fd628c673c911db1801b3356c364935e
tasveerfatama <tasveerfatema135@gmail.com>
2024-04-02 01:39:34 -0700

    numandmath.js

---
+++ numandmath.js
@@ -0,0 +1,24 @@
+const score = 400
+console.log(score);
+
+const balance = new Number (100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2)) ;
+
+const otherNumber = 123.567
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString( 'en-IN'));
+
+// +++++++++++++++++maths ++++++++++++++++++++++++++++
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.6));
+
+console.log(Math.min(3,4,5,6,7,8));
+console.log(Math.max(3,4,5,6,7,8));
