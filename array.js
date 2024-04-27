0f23bba83bb28dc8f04d2f6fd1d081e76f921606
tasveerfatama <tasveerfatema135@gmail.com>
2024-04-27 04:32:11 -0700

    array.js

--- array.js
+++ array.js
@@ -21,5 +21,56 @@ let isLoggedIn = false
 let state;
 
 console.log(typeof isLoggedIn);
+let score = "3333"
+
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log( typeof valueInNumber);
+
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+//add string 
+
+let str1 = "varun "
+let str2 = "tasveer"
+
+let str3 = str1+str2
+console.log(str3);
+
+//coparision
+
+console.log("2">1);
+console.log("2">1);
+console.log("2">1);
+console.log(null >= 0);
+console.log(null == 0);
+console.log(null > 0);
+console.log(undefined >= 0);
+console.log(undefined == 0);
+console.log(undefined > 0);
+
+// === 
+
+console.log("2"=== 2);
+
+
+//REference (non primitve)
+// Array, objects , Functions
+
+const heros = ["shaktiman","gandhi","doga"];
+
+let myObj = {
+    name : "tasveer",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+
 
 
