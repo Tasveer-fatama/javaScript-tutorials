246222319e1dc6616cd9fcf5931786df3d35f9ea
tasveerfatama <tasveerfatema135@gmail.com>
2024-04-28 03:23:21 -0700

    numandmath.js

--- numandmath.js
+++ numandmath.js
@@ -1,24 +1,29 @@
-const score = 400
+const score = 400 
 console.log(score);
-
-const balance = new Number (100)
+const balance = new Number(100)
 console.log(balance);
 
 console.log(balance.toString().length);
-console.log(balance.toFixed(2)) ;
+console.log(balance.toFixed(2));
 
-const otherNumber = 123.567
-console.log(otherNumber.toPrecision(4));
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(3));
 
-const hundreds = 1000000
-console.log(hundreds.toLocaleString( 'en-IN'));
+const hundreds = 100000000000
+console.log(hundreds.toLocaleString('en-IN'));
 
-// +++++++++++++++++maths ++++++++++++++++++++++++++++
+//+++++++++++++++++++ Math ++++++++++++++++++
 
+console.log(Math);
 console.log(Math.abs(-4));
-console.log(Math.round(4.6));
+console.log(Math.round(4.3));
 console.log(Math.ceil(4.2));
-console.log(Math.floor(4.6));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,7,4,8,6,2,9,0,3));
+console.log(Math.max(4,5,6,7,8,9,9));
 
-console.log(Math.min(3,4,5,6,7,8));
-console.log(Math.max(3,4,5,6,7,8));
+console.log(Math.random());
+console.log(Math.random()*10);
+
+
+
