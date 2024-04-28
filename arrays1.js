e95bea5fb5cdc1f1301424707f0769e73ff66e88
tasveerfatama <tasveerfatema135@gmail.com>
2024-04-28 06:50:55 -0700

    arrays1.js

---
+++ arrays1.js
@@ -0,0 +1,26 @@
+const  heros = ["punit","salman","sharukh"]
+const  heroeins = ["katrina","aishwriya","nehakakkar"]
+ 
+// heros.push(heroeins)
+// console.log(heros);
+
+//  const myheros = heros.concat(heroeins)
+// console.log(heros);
+
+// const myallheros = [...heroeins,...heros]
+// console.log(myallheros);
+
+// const realnumber = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+// const myrealnumber = realnumber.flat(Infinity)
+// console.log(myrealnumber);
+
+
+// console.log(Array.isArray("tasveer"));
+// console.log(Array.from("tasveer"));
+
+let score1 = 200
+let score2 = 300
+let score3 = 400
+
+console.log(Array.of(score1,score2,score3));
