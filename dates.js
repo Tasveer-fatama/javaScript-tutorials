a31bc3cf4c11c0cce9b359ab7a99552b2636209a
tasveerfatama <tasveerfatema135@gmail.com>
2024-04-28 03:54:05 -0700

    dates.js

--- dates.js
+++ dates.js
@@ -1,18 +1,12 @@
 let myDate = new Date()
-console.log(myDate);
 console.log(myDate.toString());
 console.log(myDate.toDateString());
 console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString());
 
-let currentDate = new Date ("01-14-2023")
-console.log(currentDate.toLocaleDateString());
+
+let mycreatedDate = new Date ("2023-01-14")
+console.log(mycreatedDate.toDateString());
 
 let myTimeStamp = Date.now()
-console.log(myTimeStamp);
-console.log(currentDate.getTime());
-console.log(Math.floor (Date.now()/1000));
 
-let newDates= new Date()
-console.log(newDates);
-console.log(newDates.getMonth());
-console.log(newDates.getDay());
