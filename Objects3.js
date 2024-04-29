41150146a70ab761c39755dc5b1cba143267e30f
tasveerfatama <tasveerfatema135@gmail.com>
2024-04-29 12:18:31 -0700

    Objects3.js

---
+++ Objects3.js
@@ -0,0 +1,22 @@
+//  d structuring 
+
+const course ={
+    coursename : "js in hindi ",
+    price: "999",
+    courseInstructuor : "tasveer"
+}
+
+// course.courseInstructuor
+const {courseInstructuor}= course
+console.log(courseInstructuor);
+
+// API 
+
+// jeason 
+
+// {
+// "name" : "tasveer ",
+// "coursename": "js in hindi",
+// "price ": "free"
+
+// }
