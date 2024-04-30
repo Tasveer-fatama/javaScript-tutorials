c3f426f7af136af3eb4c6c59846f07dab892db5f
tasveerfatama <tasveerfatema135@gmail.com>
2024-04-30 03:45:55 -0700

    IIFE.js

--- IIFE.js
+++ IIFE.js
@@ -6,3 +6,25 @@
 ((name)=>{
     console.log(`DB COONECT ${name}`);
 })('tasveer');
+
+// Arrow
+
+const user = {
+    username : "tasveer",
+    price : 999,
+    welcomemessage: function(){
+        console.log(`${this.username}, welcome our site` );
+    }
+}
+user.welcomemessage()
+user.username = "tassu"
+user.welcomemessage()
+
+//arrow
+ 
+// const addtwo = (num1,num2)=>{
+//     return num1+num2
+// }
+const addtwo = (num1,num2)=>({username:"tassu"})
+
+
